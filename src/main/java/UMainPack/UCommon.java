103a41770f9feca489951a02b2878a50539cef6c
ULTRA <goof07@inbox.ru>
2019-04-16 17:52:56 +0300

    test

--- src/main/java/UMainPack/UCommon.java
+++ src/main/java/UMainPack/UCommon.java
@@ -19,7 +19,7 @@ public class UCommon {
     static String javaHome = System.getProperty("java.home");
     static String sysdir = System.getenv("WINDIR");
     static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-    start String test7;
+    start String test8;
     
     static String md5(String st) {
         MessageDigest messageDigest = null;
